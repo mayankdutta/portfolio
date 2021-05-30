4c6fdbd5ef932e2b6234eb491a8d228acd0d315d
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-05-30 14:01:26 +0530

    main page designed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import Main from "./pages/Main";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
